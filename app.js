d26f950a0b5b4925cc7985daec5e36f5ebc02edc
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-09-02 19:19:06 +0500

    final commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// Get main div id which is parent of all
 var mainDiv = document.getElementById("mainCont");
 var data =[
    
@@ -412,59 +412,83 @@ var data =[
     }
 ]
 
+// Create div for modal and some text init
 var modalDivMain = document.createElement("div");
 modalDivMain.setAttribute("class", "modal-divmain")
 
 var modalDiv = document.createElement("div")
 modalDiv.setAttribute("class", "modal-div")
-
+modalDiv.setAttribute("id", "top")
 
 mainDiv.appendChild(modalDivMain)
 modalDivMain.appendChild(modalDiv)
 
+var modalDivP = document.createElement("p")
+modalDivP.setAttribute("onClick", "closer()")
+modalDivP.setAttribute("class", "close")
+var modalDivPTxt = document.createTextNode("X");
+var modalDivH1 = document.createElement("h1");
+var modalDivH1Txt = document.createTextNode("More Details")
+var modalDetailDiv = document.createElement("div")
+modalDetailDiv.setAttribute("class", "x")
 
 
 
+modalDiv.appendChild(modalDivP);
+modalDivP.appendChild(modalDivPTxt)
+modalDiv.appendChild(modalDivH1);
+modalDivH1.appendChild(modalDivH1Txt);
+modalDiv.appendChild(modalDetailDiv)
 
+
+// Call a function which close modal when click on X
+function closer() {
+  modalDivMain.style.display="none"
+}
+
+// Create first div which is child of main div consisits of teams name
 var firstDiv = document.createElement("div");
 firstDiv.setAttribute("class", "team-div")
 var firstDivH1 = document.createElement("h1");
 var firstDivH1Txt = document.createTextNode("Teams")
 
-
+// Create second div which is child of main div consisits of code
 var secondDiv = document.createElement("div");
 secondDiv.setAttribute("class", "code-div")
 var secondDivH1 = document.createElement("h1");
 var secondDivH1Txt = document.createTextNode("Code")
 
-
+// Create third div which is child of main div consisits of continent
 var thirdDiv = document.createElement("div");
 thirdDiv.setAttribute("class", "cont-div")
 var thirdDivH1 = document.createElement("h1");
 var thirdDivH1Txt = document.createTextNode("Continent")
 
-
+// Create second div which is child of main div consisits of details text
 var fourthDiv = document.createElement("div");
 fourthDiv.setAttribute("class", "detail-div")
 var fourthDivH1 = document.createElement("h1");
 var fourthDivH1Txt = document.createTextNode("More Details")
 
 
-
+// Append first div
 mainDiv.appendChild(firstDiv);
 firstDiv.appendChild(firstDivH1);
 firstDivH1.appendChild(firstDivH1Txt);
 
+// Append second div
 mainDiv.appendChild(secondDiv)
 secondDiv.appendChild(secondDivH1);
 secondDivH1.appendChild(secondDivH1Txt);
 
 
+// Append third div
 mainDiv.appendChild(thirdDiv)
 thirdDiv.appendChild(thirdDivH1);
 thirdDivH1.appendChild(thirdDivH1Txt);
 
 
+// Append fourth div
 mainDiv.appendChild(fourthDiv)
 fourthDiv.appendChild(fourthDivH1);
 fourthDivH1.appendChild(fourthDivH1Txt);
@@ -473,33 +497,26 @@ fourthDivH1.appendChild(fourthDivH1Txt);
 
 
 
-
+// Run loop to get data from array of object and store data in p tag by creating in loop
 for (var key in data){
     // console.log(data[key].name);
-
-    // if(typeof (data[key]) === "object") {
-    //     console.log( data[key].assoc)
-    // }
-
-    // if(typeof (data[key]).assoc === "object") {
-    //     console.log( data[key].assoc.continental)
-
-    // }
     var firstDivP = document.createElement("p");
+    firstDivP.setAttribute("class", "first")
     var firstDivPTxt = document.createTextNode(data[key].name)
 
     var secondDivP = document.createElement("p");
+    secondDivP.setAttribute("class", "cd")
     var secondDivPTxt = document.createTextNode(data[key].code)
 
     var thirdDivP = document.createElement("p");
+    thirdDivP.setAttribute("class", "contad")
     var thirdDivPTxt = document.createTextNode(data[key].continent)
  
-    var fourthDivP = document.createElement("p");
+    var fourthDivP = document.createElement("a");
     fourthDivP.setAttribute("onClick", `details("${data[key].name}")`)
+    fourthDivP.setAttribute("href", "#top")
+    fourthDivP.setAttribute("class", "link")
     var fourthDivPTxt = document.createTextNode("Get Details")
-
-
-
 
 
 firstDiv.appendChild(firstDivP);
@@ -518,26 +535,19 @@ fourthDivP.appendChild(fourthDivPTxt);
 console.log(mainDiv);
 
 
-
+// Create function which give further detail of teams in modal 
 function details(names) {
+  modalDivMain.style.display="block"
 
     for (var key in data){
 
-    if(data[key].name === names) {
-        console.log(data[key])
-        break;
-    }
+      if(data[key].name === names) {
+          console.log(data[key].name)
+          modalDetailDiv.innerHTML = "<h2>  Association:  </h2>" +
+          "<h3>"+ "Key : " + data[key].assoc.key  +"</h3>" +  "<h3>" +
+          "Name : " + data[key].assoc.name  +"</h3>" +  "<h2>  Continental:  </h2>" +
+          "<h3> Name : " + data[key].assoc.continental.name  +"</h3>" + "<h3> Code : " + data[key].assoc.continental.code  +"</h3>"
+      }
 
-      if(typeof (data[key]) === "object") {
-         console.log( data[key].assoc)
-     }
-
-    if(typeof (data[key]).assoc === "object") {
-        console.log( data[key].assoc.continental)
-
-    }
-}
-modalDivMain.style.display="block"
-
-    
+    }   
 } 
